495c915c56283a776fecda32b79a65cfdd4e2374
John-John Tedro <udoprog@tedro.se>
2019-06-07 01:17:10 +0200

    Change string signatures

--- src/api/linux/mod.rs
+++ src/api/linux/mod.rs
@@ -84,7 +84,7 @@ impl GtkSystrayApp {
         self.menu.show_all();
     }
 
-    pub fn add_menu_entry(&self, item_idx: u32, item_name: &String) {
+    pub fn add_menu_entry(&self, item_idx: u32, item_name: &str) {
         let mut menu_items = self.menu_items.borrow_mut();
         if menu_items.contains_key(&item_idx) {
             let m: &gtk::MenuItem = menu_items.get(&item_idx).unwrap();
@@ -103,7 +103,7 @@ impl GtkSystrayApp {
         self.menu.show_all();
     }
 
-    pub fn set_icon_from_file(&self, file: &String) {
+    pub fn set_icon_from_file(&self, file: &str) {
         let mut ai = self.ai.borrow_mut();
         ai.set_icon_full(file, "icon");
     }
@@ -137,7 +137,7 @@ impl Window {
         }
     }
 
-    pub fn add_menu_entry(&self, item_idx: u32, item_name: &String) -> Result<(), SystrayError> {
+    pub fn add_menu_entry(&self, item_idx: u32, item_name: &str) -> Result<(), SystrayError> {
         let n = item_name.clone();
         run_on_gtk_thread(move |stash: &GtkSystrayApp| {
             stash.add_menu_entry(item_idx, &n);
@@ -152,7 +152,7 @@ impl Window {
         Ok(())
     }
 
-    pub fn set_icon_from_file(&self, file: &String) -> Result<(), SystrayError> {
+    pub fn set_icon_from_file(&self, file: &str) -> Result<(), SystrayError> {
         let n: String = file.clone();
         run_on_gtk_thread(move |stash: &GtkSystrayApp| {
             stash.set_icon_from_file(&n);
